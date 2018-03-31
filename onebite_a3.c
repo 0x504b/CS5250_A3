92347c068e605f83793db7cbcbe7679c4ac82109
tshark <e0146260@u.nus.edu>
2018-03-31 10:36:27 -0700

    last commit for onebite

--- onebite_a3.c
+++ onebite_a3.c
@@ -10,20 +10,16 @@
 
 #define MAJOR_NUMBER 61
 
-/* forward declaration */
-
 int onebyte_open(struct inode *inode, struct file *filep);
 int onebyte_release(struct inode *inode, struct file *filep);
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
 static void onebyte_exit(void);
 
-/* definition of file_operation structure */
-
 struct file_operations onebyte_fops = {
-     read:     onebyte_read,
-     write:    onebyte_write,
-     open:     onebyte_open,
+     read: onebyte_read,
+     write: onebyte_write,
+     open: onebyte_open,
      release: onebyte_release
 };
 
@@ -31,94 +27,69 @@ char *onebyte_data = NULL;
 
 int onebyte_open(struct inode *inode, struct file *filep)
 {
-     return 0; // always successful
+     return 0;
 }
-
 int onebyte_release(struct inode *inode, struct file *filep)
 {
-     return 0; // always successful
+     return 0;
 }
-
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
-     /*please complete the function on your own*/
-     int satoshi_1 = 0;
-
-     satoshi_1 = copy_to_user(buf, onebyte_data, 1);
-
-     if(satoshi_1 == 0)
-     {
-          return 1;
-     }
-     else 
-     {
-          printk(KERN_ALERT "Did not succeed READ...\n");
-          return -EFAULT;
+     int bytes_read = 0;
+     
+     if(*buf != 0){
+          printk(KERN_ALERT "READ FAILED: char buffer not empty!\n");
+          return 0; 
      }
 
+     copy_to_user(buf, onebyte_data, sizeof(char));
+     bytes_read ++;
+     printk(KERN_ALERT "READ SUCCESS: %c\n", *onebyte_data);
+     return bytes_read;
 }
-
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-     /*please complete the function on your own*/
-     int satoshi_2 = 0;
+     int bytes_write = 0;     
 
-     satoshi_2 = copy_from_user(onebyte_data, buf, 1);
+     if(count > sizeof(char))
+     {
+          printk(KERN_ALERT "WRITE FAILED: No space left on device\n");
+          return -ENOSPC; 
+     }
 
-     if(satoshi_2 == 0)
-     {
-          return 1;
-     }
-     else
-     {
-          printk(KERN_ALERT "Did not succeed WRITE...\n");
-          return -EFAULT;
-     }
+     copy_from_user(onebyte_data, buf, sizeof(char));
+     bytes_write ++;
+     printk(KERN_ALERT "WRITE SUCCESS: %c\n", *onebyte_data);
+     return bytes_write;
 }
 
 static int onebyte_init(void)
 {
      int result;
-     // register the device
      result = register_chrdev(MAJOR_NUMBER, "onebyte", &onebyte_fops);
      if (result < 0) {
-
+          return result;
      }
-
-     return result;
-
-     // allocate one byte of memory for storage
-     // kmalloc is just like malloc, the second parameter is
-     // the type of memory to be allocated.
-     // To release the memory allocated by kmalloc, use kfree.
-
      onebyte_data = kmalloc(sizeof(char), GFP_KERNEL);
 
      if (!onebyte_data) {
           onebyte_exit();
-          // cannot allocate memory
-          // return no memory error, negative signify a failure
+          return -ENOMEM;
      }
 
-     return -ENOMEM;
-
-     // initialize the value to be X
      *onebyte_data = 'X';
-     printk(KERN_ALERT "This is a onebyte device module\n");
 
+     printk(KERN_ALERT "This is a onebyte device module\n");
      return 0;
 }
 
 static void onebyte_exit(void)
 {
-     // if the pointer is pointing to something
-     if (onebyte_data)
-     {
-          // free the memory and assign the pointer to NULL
+     if (onebyte_data) {
           kfree(onebyte_data);
+          onebyte_data = NULL;
      }
-     onebyte_data = NULL;
-     // unregister the device
+
      unregister_chrdev(MAJOR_NUMBER, "onebyte");
      printk(KERN_ALERT "Onebyte device module is unloaded\n");
 }
